c69fe204395fec9c741de724fc52c56bf291c6be
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2022-05-09 11:30:13 -0400

    Update RunDylib.swift

--- RunDylib.swift
+++ RunDylib.swift
@@ -2,12 +2,25 @@ import Foundation
 
 typealias addFunc = @convention(c) (CInt,CInt) -> CInt
 
-let myhandle = dlopen("[file_path_to_dylib]", RTLD_NOW)
-sleep(3)
-if (myhandle != nil){
-    let x = dlsym(myhandle, "[export_function_name_here]")//for calling main in C built dylibs, use _main
-    if (x != nil){
-        print(x)
-    }
-    dlclose(myhandle)
+do {
+let dispatch = DispatchQueue.global(qos: .background)
+        let myhandle = dlopen("file_path_to_dylib", RTLD_GLOBAL)
+        if (myhandle != nil){
+                dispatch.async{
+                    DispatchQueue.main.async {
+                        let x = dlsym(myhandle, "dylib_export_function_name_here")//for calling main in C built dylibs, use _main
+                if (x != nil){
+                       print(x)
+               }
+                dlclose(myhandle)
+                    }
+
+
+                }
+
+        }
+} catch let(err){
+        print(err)
 }
+
+RunLoop.main.run()
